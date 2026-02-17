bd5cdb2256c36bfdea9219a2d6ce720d643a5ab9
Zainab-Raza <27100435@lums.edu.pk>
2026-02-17 15:59:42 +0500

    Adding blue to color

--- app/src/main/java/com/example/catnip/Shape.java
+++ app/src/main/java/com/example/catnip/Shape.java
@@ -4,9 +4,11 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
+    protected String color;
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        this.color = "blue";
     }
 }
